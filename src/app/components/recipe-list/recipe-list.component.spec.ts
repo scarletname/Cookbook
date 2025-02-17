14a7518f54a1c7b7b05dc5c106d7e857abbd33bf
qq_ruza <scarletname@yandex.ru>
2025-02-18 02:36:00 +0300

    recipe-list component updated (html, scss, ts)

--- src/app/components/recipe-list/recipe-list.component.spec.ts
+++ src/app/components/recipe-list/recipe-list.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { RecipeListComponent } from './recipe-list.component';
+interface Recipe {
+  id: string;
+  title: string;
+  ingredients: { name: string; amount: number; unit: string }[];
+  description: string;
+  imageUrl?: string;
+  createdAt: Date;
+}
 
-describe('RecipeListComponent', () => {
-  let component: RecipeListComponent;
-  let fixture: ComponentFixture<RecipeListComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RecipeListComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(RecipeListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    {
+      id: '1',
+      title: 'Блины',
+      ingredients: [],
+      description: '---',
+      createdAt: new Date()
+    },
+    {
+      id: '2',
+      title: 'Панкейки',
+      ingredients: [],
+      description: '---',
+      createdAt: new Date()
+    }
+  ];
+}
