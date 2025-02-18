4612e30117d98b0e64e90dfcdc628ba959dfcd2b
qq_ruza <scarletname@yandex.ru>
2025-02-18 06:59:51 +0300

    recipe-form component updated (html, scss, ts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Standalone-компонент
-  imports: [RecipeListComponent], // Подключаем `RecipeListComponent`
+  standalone: true,
+  imports: [RecipeListComponent, RecipeFormComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule  } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-recipe-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatInputModule, MatButtonModule, MatCardModule, MatSelectModule],
   templateUrl: './recipe-form.component.html',
-  styleUrl: './recipe-form.component.scss'
+  styleUrls: ['./recipe-form.component.scss']
 })
 export class RecipeFormComponent {
+  recipe = {
+    title: '',
+    ingredients: [{name: '', amount: null, unit: ''}],
+    description: ''
+  };
 
+  units = ['г', 'мл', 'шт.', 'ч.л.', 'ст.л'];
+
+  addIngredient(){
+    this.recipe.ingredients.push({name: '', amount: null, unit: ''});
+  }
+
+  removeIngredient(index:number) {
+    this.recipe.ingredients.splice(index, 1);
+  }
+
+  saveRecipe() {
+    console.log('Рецепт сохранен: ', this.recipe);
+  }
 }
